7d55427810782b509493789370c12628d915baac
vscode-mvp <procachprocach@gmail.com>
2026-07-13 21:16:17 +0700

    Создана основная структура

---
+++ save-response-in-txt.js
@@ -0,0 +1,21 @@
+import fs from "node:fs/promises"
+import os from "node:os"
+
+async function apiFetchAndWrite(url) {
+    try {
+        if (url) {
+    const data = await fetch(url).then(response => response.json())
+        }
+        else {
+            console.log('Произошла ошибка! Скорее всего не указан URL в вызове (как это сделать читай в инструкции https://)')
+        }
+
+    await fs.appendFile('jsonResponses.txt', JSON.stringify(data) + os.EOL, 'utf-8')
+    console.log(`Ответ успешно сохранен в файл jsonResponses.txt рядом с главным файлом index.js!`)
+    }
+    catch(error) {
+        console.error(`Ошибка: ${error.message}`)
+    }
+}
+
+apiFetchAndWrite() // В АРГУМЕНТАХ ВАШ URL ПО КОТОРОМУ НЕОБХОДИМО ПОЛУЧИТЬ ОТВЕТ
