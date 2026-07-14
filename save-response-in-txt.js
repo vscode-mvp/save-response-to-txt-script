785a5c60d3b2fde2188712dfd9bd0683b4e36483
vscode-mvp <procachprocach@gmail.com>
2026-07-14 18:56:35 +0700

    Добавлен README.md / Попоравки в файле index.js

--- save-response-in-txt.js
+++ save-response-in-txt.js
@@ -7,7 +7,7 @@ async function apiFetchAndWrite(url) {
     const data = await fetch(url).then(response => response.json())
         }
         else {
-            console.log('Произошла ошибка! Скорее всего не указан URL в вызове (как это сделать читай в инструкции https://)')
+            console.log('Произошла ошибка! Скорее всего не указан URL в вызове (как это сделать читай в инструкции README)')
         }
 
     await fs.appendFile('jsonResponses.txt', JSON.stringify(data) + os.EOL, 'utf-8')
