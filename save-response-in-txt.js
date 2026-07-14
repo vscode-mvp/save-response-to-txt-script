41c56be9906ed3078c7859fb8bfe5c7f5d9908db
vscode-mvp <procachprocach@gmail.com>
2026-07-14 19:32:05 +0700

    bug fix: Исправлена отработка ошибок в save-response-in-txt.js

--- save-response-in-txt.js
+++ save-response-in-txt.js
@@ -3,16 +3,16 @@ import os from "node:os"
 
 async function apiFetchAndWrite(url) {
     try {
-        if (url) {
-    const data = await fetch(url).then(response => response.json())
-        }
-        else {
+        if (!url) {
             console.log('Произошла ошибка! Скорее всего не указан URL в вызове (как это сделать читай в инструкции README)')
         }
+        else {
+            const data = await fetch(url).then(response => response.json())
 
-    await fs.appendFile('jsonResponses.txt', JSON.stringify(data) + os.EOL, 'utf-8')
-    console.log(`Ответ успешно сохранен в файл jsonResponses.txt рядом с главным файлом index.js!`)
+            await fs.appendFile('jsonResponses.txt', JSON.stringify(data) + os.EOL, 'utf-8')
+            console.log(`Ответ успешно сохранен в файл jsonResponses.txt рядом с главным файлом!`)
     }
+}
     catch(error) {
         console.error(`Ошибка: ${error.message}`)
     }
